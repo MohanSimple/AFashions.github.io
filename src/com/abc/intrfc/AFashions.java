153409fcaf9d09554674eee996fa9b4a74153049
MohanSimple <61163811+MohanSimple@users.noreply.github.com>
2020-03-17 16:52:36 +0530

    Revert "AFashions"
    This reverts commit c7b5137f7b38fe6a58ae51e49a8ff950b23fe444.

--- src/com/abc/intrfc/AFashions.java
+++
@@ -1,8 +0,0 @@
-package com.abc.intrfc;
-
-public interface AFashions {
-public void get();
-public void get(int id,String name);
-public void get(int id,String name, String size);
-public void get(int id,String name, String size,int price);
-}
